1b6791bb3acbc3875f694383ba89257b1f170804
Vigneshwaran <vigneshcj001@gmail.com>
2025-09-19 20:57:39 +0530

    Data Analytics Agent

--- start.sh
+++ start.sh
@@ -7,7 +7,4 @@ pip install --upgrade pip setuptools wheel
 # Install dependencies
 pip install -r requirements.txt
 
-# Start FastAPI server
-uvicorn main:app --host 0.0.0.0 --port $PORT
-
 python main.py
